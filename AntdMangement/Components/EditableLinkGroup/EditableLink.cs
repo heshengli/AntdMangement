21bb100e216d0c22e74c3afa8b7930eb22c5c6d9
heshengli <742611727@qq.com>
2023-01-03 23:59:48 +0800

    20230103

---
+++ AntdMangement/Components/EditableLinkGroup/EditableLink.cs
@@ -0,0 +1,9 @@
+﻿namespace AntdMangement.Components
+{
+    public class EditableLink
+    {
+        public string Title { get; set; }
+        public string Href { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ AntdMangement/Components/SalesCard/SaleItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AntdMangement.Components
+{
+    public class SaleItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Total { get; set; }
+    }
+}
